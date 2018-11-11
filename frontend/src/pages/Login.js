4da03b046ff2759b4272856cb8cf437f725a17cd
Italo <italoaurelior@gmail.com>
2018-11-11 08:43:28 -0200

    First Upload

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Login.css';
+import twitterLogo from '../twitter.svg';
+
+export default class Login extends Component {
+    state = {
+        username: '',
+
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { username } = this.state;
+
+        if(!username.length) return;
+
+        localStorage.setItem('@GoTwitter:username', username);
+
+        this.props.history.push('/timeline');
+    };
+
+    handleInputChange = e => {
+        this.setState({
+            username: e.target.value,
+        });
+    };
+
+  render() {
+    return (
+        <div className="login-wrapper">
+            <img src={twitterLogo} alt="Go Twitter"/>
+            <form onSubmit={this.handleSubmit}>
+                <input 
+                value={this.state.username}
+                onChange={this.handleInputChange}
+                placeholder="Insira o nome de usuário" />
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+    );
+  }
+}
